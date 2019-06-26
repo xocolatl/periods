0de664a8c68b55dc62dd89898f2bc7e246bf5d33
Vik Fearing <vik.fearing@protonmail.com>
2019-06-26 05:18:51 +0200

    Add tests for temporal querying

--- sql/periods.sql
+++ sql/periods.sql
@@ -60,13 +60,31 @@ INSERT INTO sysver (val) VALUES ('hello');
 SELECT val FROM sysver;
 SELECT val FROM sysver_history ORDER BY system_time_start;
 
+SELECT transaction_timestamp() AS ts1 \gset
+
 UPDATE sysver SET val = 'world';
 SELECT val FROM sysver;
 SELECT val FROM sysver_history ORDER BY system_time_start;
 
+SELECT transaction_timestamp() AS ts2 \gset
+
 DELETE FROM sysver;
 SELECT val FROM sysver;
 SELECT val FROM sysver_history ORDER BY system_time_start;
+
+/* temporal queries */
+
+SELECT val FROM sysver__as_of(:'ts1') ORDER BY system_time_start;
+SELECT val FROM sysver__as_of(:'ts2') ORDER BY system_time_start;
+
+SELECT val FROM sysver__from_to(:'ts1', :'ts2') ORDER BY system_time_start;
+SELECT val FROM sysver__from_to(:'ts2', :'ts1') ORDER BY system_time_start;
+
+SELECT val FROM sysver__between(:'ts1', :'ts2') ORDER BY system_time_start;
+SELECT val FROM sysver__between(:'ts2', :'ts1') ORDER BY system_time_start;
+
+SELECT val FROM sysver__between_symmetric(:'ts1', :'ts2') ORDER BY system_time_start;
+SELECT val FROM sysver__between_symmetric(:'ts2', :'ts1') ORDER BY system_time_start;
 
 -- We can't drop the the table without first dropping SYSTEM VERSIONING because
 -- Postgres will complain about dependant objects (our view functions) before
