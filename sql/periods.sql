c1f946832ccb6c8bd74188da9f0ff2b78c01096e
Vik Fearing <vik.fearing@protonmail.com>
2019-08-02 18:01:55 +0200

    Improve comment in tests.
    Make it clearer that we're playing with a test table to make sure the
    health checks don't try to do anything.

--- sql/periods.sql
+++ sql/periods.sql
@@ -9,7 +9,8 @@ CREATE EXTENSION periods;
 
 /*
  * Test creating a table, dropping a column, and then dropping the whole thing;
- * without any periods.
+ * without any periods.  This is to make sure the health checks don't try to do
+ * anything.
  */
 CREATE TABLE beeswax (col1 text, col2 date);
 ALTER TABLE beeswax DROP COLUMN col1;
